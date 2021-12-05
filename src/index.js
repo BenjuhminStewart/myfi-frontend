b56cbc82cd7ef5970cdfaa804111c3909c0aa50b
Benjamin Stewart <82689821+BenjuhminStewart@users.noreply.github.com>
2021-12-05 01:08:18 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import { CookiesProvider } from "react-cookie";
+
 ReactDOM.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
